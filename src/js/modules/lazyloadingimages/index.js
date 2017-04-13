dc3cbb06f8c4f8f7e5d07de7c0be7c55ad5d1dc9
Marco Prins <marco.prins@icloud.com>
2017-04-13 23:34:10 +0200

    code formatting

--- src/js/modules/lazyloadingimages/index.js
+++ src/js/modules/lazyloadingimages/index.js
@@ -4,10 +4,12 @@ import loadimage from './imageloader';
 const init = () => {
 
   const els = document.querySelectorAll('div.image');
+
   Observer.createObserver({
     callback: loadimage,
     threshold: 1,
   });
+
   els.forEach(el => Observer.getObserver().observe(el));
 
 };
